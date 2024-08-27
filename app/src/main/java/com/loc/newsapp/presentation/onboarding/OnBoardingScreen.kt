07b35d8537f57d84d9c12423aa7764ef57d82392
omeryttnc <omeryttnc@gmail.com>
2024-08-27 01:39:08 +0100

    day 3

---
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,9 @@
+package com.loc.newsapp.presentation.onboarding
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun OnBoardingScreen(modifier: Modifier = Modifier) {
+    https://www.youtube.com/watch?v=ZwmFEFRLrOA&list=PLzZEuVaFb9Exi-pc8qtHBrrLg8bUn-TP6&index=3
+}
